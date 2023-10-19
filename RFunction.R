46cad7bead2cfead1ab38936346c3bf4a987e31d
annescharf <ascharf@ab.mpg.de>
2023-10-19 14:13:13 +0200

    improved duplicate removal and thinning

--- RFunction.R
+++ RFunction.R
@@ -1,18 +1,14 @@
 library('move2')
 library('keyring')
 library('lubridate')
+library("dplyr")
 
 # remains to update: 
-
-# 1. the setting duplicates handling shall give only either "first" or "last" (in second mt_filter_unique), now it is not used and set to "last" fix --> activated
-
 # 2. EURING_1 und EURING_3 options shall be added, but as they dont work and the frontend does not allow it for selection, not yet --> probably temporary bug, now it works
-
-# 3. add fix for deployment_id (to be named from indivdual_local_identifier and tag_local_identifier) - there should be some code from Bart --> fixed
 
 # 4. select animals - should be changed with Clemens: animals==0 for all animals also in future added ones (make ticket that this info shall be written somewhere or additinal check-box) --> made ticket
 
-rFunction = function(data=NULL, username,password,study,select_sensors,incl_outliers=FALSE,minarg=FALSE,animals=NULL,thin=FALSE,thin_numb=6,thin_unit="hours",timestamp_start=NULL,timestamp_end=NULL,duplicates_handling="first",event_reduc=NULL, ...) {
+rFunction = function(data=NULL, username,password,study,select_sensors,incl_outliers=FALSE,minarg=FALSE,animals=NULL,thin=FALSE,thin_numb=6,thin_unit="hours",timestamp_start=NULL,timestamp_end=NULL,event_reduc=NULL, ...) {
 
   options("keyring_backend"="env")
   movebank_store_credentials(username,password)
@@ -120,7 +116,7 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
     #download
     locs <- do.call(movebank_download_study,arguments)
   
-    # quality check: cleaved, time ordered, non-emtpy, non-duplicated
+    # quality check: cleaved, time ordered, non-emtpy, non-duplicated (dupl get removed further down in the code)
     if(!mt_is_track_id_cleaved(locs))
     {
       logger.info("Your data set was not grouped by individual/track. We regroup it for you.")
@@ -138,34 +134,40 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
       logger.info("Your data included empty points. We remove them for you.")
       locs <- dplyr::filter(locs, !sf::st_is_empty(locs))
     }
-
+    ## for some reason, sometimes either lat or long are NA, as one still has a value it does not get removed with the excluding empty, here is what I came up with:
+    crds <- sf::st_coordinates(locs)
+    rem <- unique(c(which(is.na(crds[,1])),which(is.na(crds[,2]))))
+    if(length(rem)>0){
+      locs <- locs[-rem,]
+    }
+    
     # rename track_id column to always combination of individual+tag so it is consistent and informative across studies. Used same naming as in "mt_read()"
     # suggestion form Bart: maybe better use "animalName (dep_id:358594)" because it could happen that the same indiv gets tagged with the same tag in 2 different years. If using "indv_tag", tracks could get merged together that are actually different deployments
     # ToDo: decide on column name e.g. "individual_name_deployment_id" and renaming e.g. "indivName (deploy_id:084728)"
     locs <- locs |> mutate_track_data(individual_name_deployment_id = paste0(mt_track_data(locs)$individual_local_identifier ," (deploy_id:",mt_track_data(locs)$deployment_id,")")) # "deploy_id" or some other abbreviation that makes sense
     idcolumn <- mt_track_id_column(locs) # need to get track id column before changing it
-    locs <- mt_as_event_attribute(locs,"individual_name_deployment_id") ## bug: this function turns the track data table into a data.frame. Bart is looking into it. But this should not cause any errors downstream.
+    locs <- mt_as_event_attribute(locs,"individual_name_deployment_id")
     locs <- mt_set_track_id(locs, "individual_name_deployment_id")
-    locs <- mt_as_track_attribute(locs,idcolumn)
+    locs <- mt_as_track_attribute(locs,all_of(idcolumn)) # when changing the track_id column, the previous one stays in the event table, but gets removed from track table (which makes sense), but putting it back as in this case it will always work
   
-    # remove duplicates without user interaction, start with select most-info row, then last (or first)
+    # remove duplicates without user interaction, start with select most-info row
     if (!mt_has_unique_location_time_records(locs))
     {
       n_dupl <- length(which(duplicated(paste(mt_track_id(locs),mt_time(locs)))))
       logger.info(paste("Your data has",n_dupl, "duplicated location-time records. We removed here those with less info and then select the first if still duplicated."))
-      locs <- mt_filter_unique(locs,criterion="subsets")
-      locs <- mt_filter_unique(locs,criterion=duplicates_handling)
-      
-      logger.info(paste("Your data set contained",n_dupl,"duplicated locations."))
-      if (n_dupl>0) logger.info(paste("The duplicated locations were removed according to selection for most information if true subsets of the attributes or by selecting the",duplicates_handling,"location each (according to your selected setting)."))
+      ## this piece of code keeps the duplicated entry with least number of columns with NA values
+      locs <- locs %>% rowwise() %>%
+        mutate(n_na = sum(is.na(cur_data()))) %>%
+        arrange(n_na) %>%
+        mt_filter_unique(criterion='first') # this always needs to be "first" because the duplicates get ordered according to the number of columns with NA. 
     }
     
-    #thinning to first location of given intervals (thus, resulting time lag can be shorter some times)
+    #thinning to first location of given time windows (thus, resulting time lag can be shorter some times)
     if (thin==TRUE) 
     {
       logger.info(paste("Your data will be thinned as requested to one location per",thin_numb,thin_unit))
       locs <- mt_filter_per_interval(locs,criterion="first",unit=paste(thin_numb,thin_unit))
-      #locs <- locs[!duplicated(paste(mt_track_id(locs),round_date(mt_time(locs), paste0(thin_numb," ",thin_unit)))),]
+      locs <- locs[-1,] ## the thinning happens within the time window, so the 1st location is mostly off. After the 1st location the intervals are regular if the data allow for it
     }
     
     #make names
