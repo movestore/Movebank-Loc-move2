8969fd535d57ed9b6d1f7cd9dcda20aaad0b768c
annescharf <ascharf@ab.mpg.de>
2026-05-18 23:13:26 +0200

    indent code

--- RFunction.R
+++ RFunction.R
@@ -150,7 +150,7 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
           # 
           #     }
           # else{
-
+          
           #download
           locs <- do.call(movebank_download_study,arguments)
           
@@ -244,7 +244,7 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
           #make names
           # names(locs) <- make.names(names(locs),allow_=TRUE)
           mt_track_id(locs) <- make.names(mt_track_id(locs),allow_=TRUE)
-         
+          
           # combine with other input data (move2!)
           if (!is.null(data)){
             if (!st_crs(data)==st_crs(locs)){
@@ -266,22 +266,22 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
                   ~unlist(purrr::map(.x, paste, collapse=","))))
               }
             }
-        }else{
-          result <- locs
-          ## unlisting track data columns of class list
-          if(any(sapply(mt_track_data(result), is_bare_list))){
-            ## reduce all columns were entry is the same to one (so no list anymore)
-            result <- result |> mutate_track_data(across(
-              where( ~is_bare_list(.x) && all(purrr::map_lgl(.x, function(y) 1==length(unique(y)) ))), 
-              ~do.call(vctrs::vec_c,purrr::map(.x, head,1))))
+          }else{
+            result <- locs
+            ## unlisting track data columns of class list
             if(any(sapply(mt_track_data(result), is_bare_list))){
-              ## transform those that are still a list into a character string
+              ## reduce all columns were entry is the same to one (so no list anymore)
               result <- result |> mutate_track_data(across(
-                where( ~is_bare_list(.x) && any(purrr::map_lgl(.x, function(y) 1!=length(unique(y)) ))), 
-                ~unlist(purrr::map(.x, paste, collapse=","))))
+                where( ~is_bare_list(.x) && all(purrr::map_lgl(.x, function(y) 1==length(unique(y)) ))), 
+                ~do.call(vctrs::vec_c,purrr::map(.x, head,1))))
+              if(any(sapply(mt_track_data(result), is_bare_list))){
+                ## transform those that are still a list into a character string
+                result <- result |> mutate_track_data(across(
+                  where( ~is_bare_list(.x) && any(purrr::map_lgl(.x, function(y) 1!=length(unique(y)) ))), 
+                  ~unlist(purrr::map(.x, paste, collapse=","))))
+              }
             }
           }
-        }
           
           ## remove all attributes that contain NAs in all rows
           na_cols <- result %>%
@@ -303,9 +303,9 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
             select(where(~ !all(is.na(.)))) %>% 
             select_track_data(where(~ !all(is.na(.))))
           
-          }
+        }
         # }
-          },silent=TRUE)
+      },silent=TRUE)
   }
   
   if(!exists("result"))
@@ -315,14 +315,14 @@ rFunction = function(data=NULL, username,password,study,select_sensors,incl_outl
   }
   
   if(!is.null(result)){
-  ## create file with metadata for download
-  attrList <- c("study_id", "name", "taxon_ids", "principal_investigator_name", "contact_person_name", "citation", "license_terms", "license_type")
-  metadata <- result %>% mt_track_data() %>% select(any_of(attrList)) %>% distinct(.keep_all = TRUE)
-  metadata$download_date <- Sys.Date()
-  metadata_csv <- data.frame(names(metadata),t(metadata))
-  if(nrow(metadata)>=1){
-  write.table(metadata_csv,appArtifactPath("citation_metadata.csv"), row.names = F, col.names=F)
-  }
+    ## create file with metadata for download
+    attrList <- c("study_id", "name", "taxon_ids", "principal_investigator_name", "contact_person_name", "citation", "license_terms", "license_type")
+    metadata <- result %>% mt_track_data() %>% select(any_of(attrList)) %>% distinct(.keep_all = TRUE)
+    metadata$download_date <- Sys.Date()
+    metadata_csv <- data.frame(names(metadata),t(metadata))
+    if(nrow(metadata)>=1){
+      write.table(metadata_csv,appArtifactPath("citation_metadata.csv"), row.names = F, col.names=F)
+    }
   }
   
   return(result)
